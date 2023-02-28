2b8c382ee4a1a79e11b29db57dbe18a4c4f1a1a4
Onaivio <107544743+Onaivio@users.noreply.github.com>
2023-02-28 19:41:19 +0100

    ..

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom"
 import "./notFound.css"
+import ErrorPhoto from "../../images/error.png"
 
 const NotFound = () => {
   return (
     <section>
       <div className="container notfound__container">
-        {/* <img src="../../images/404 error with portals-pana.svg" className="404-Error" alt="Page Not Found" /> */}
+        <img src={ErrorPhoto} className="404-Error" alt="Page Not Found" />
         <h1>404 Error</h1>
         <h2>Page Not Found</h2>
         <Link to = "/"  className = "btn">Go Back Home</Link>
